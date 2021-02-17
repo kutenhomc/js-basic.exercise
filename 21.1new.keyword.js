33da34e336a4faefc4a05c4a0002d417e2e31564
TRUONG QUOC CUONG <doimat12@gmail.com>
2021-02-17 13:12:45 +0900

    new-keyword

---
+++ 21.1new.keyword.js
@@ -0,0 +1,19 @@
+/**
+ * Tạo một đối tượng student gồm các property name, sex, age
+ * Example:
+  name: "CodersX",
+  sex: "Male",
+  age: 1
+ * Viết hàm showInfo trả về object đó
+*/
+
+var student = {
+    name :"CodersX",
+    age:1,
+    sex:"Male"
+    }
+    
+    function showInfo(obj) {
+      // viết code ở đây
+    //   return obj;
+    }
